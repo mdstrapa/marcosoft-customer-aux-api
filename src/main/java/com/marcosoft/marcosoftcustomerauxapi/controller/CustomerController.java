dd7c4a6ac826a010e199d98d6c82388b43f3baeb
Marcos Daniel Strapazon <marcos_strapazon@sicredi.com.br>
2024-02-07 22:39:34 -0300

    feat: update and put operations added

--- src/main/java/com/marcosoft/marcosoftcustomerauxapi/controller/CustomerController.java
+++ src/main/java/com/marcosoft/marcosoftcustomerauxapi/controller/CustomerController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("customer")
 @AllArgsConstructor
@@ -17,6 +19,29 @@ public class CustomerController {
     @PostMapping
     public ResponseEntity<Customer> save(@RequestBody Customer newCustomer){
         return ResponseEntity.status(HttpStatus.CREATED).body(customerService.save(newCustomer));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Customer> findById(@PathVariable Long id){
+        return customerService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
+
+    @GetMapping("all")
+    public ResponseEntity<List<Customer>> findAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.findAll());
+    }
+    
+    @PutMapping()
+    public ResponseEntity<Customer> update(@RequestBody Customer customer){
+        return ResponseEntity.status(HttpStatus.CREATED).body(customerService.save(customer));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id){
+        customerService.deleteById(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("");
     }
 
 }
--- src/main/java/com/marcosoft/marcosoftcustomerauxapi/service/CustomerService.java
+++ src/main/java/com/marcosoft/marcosoftcustomerauxapi/service/CustomerService.java
@@ -5,6 +5,9 @@ import com.marcosoft.marcosoftcustomerauxapi.repository.CustomerRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class CustomerService {
@@ -16,4 +19,21 @@ public class CustomerService {
         newCustomer.setSurname(newCustomer.getSurname().toUpperCase());
         return customerRepository.save(newCustomer);
     }
+
+    public Optional<Customer> findById(Long id){
+        return customerRepository.findById(id);
+    }
+
+    public List<Customer> findAll(){
+        return customerRepository.findAll();
+    }
+
+    public void deleteById(Long id){
+        customerRepository.deleteById(id);
+    }
+
+    public Customer update(Customer customer){
+        return customerRepository.save(customer);
+    }
+
 }
